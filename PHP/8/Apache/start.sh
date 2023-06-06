aa9b2aff4e313cb45d2d6a54948713d3971043ff
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2023-06-05 22:35:55 -0300

    Que vuele todo

--- PHP/8/Apache/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-if [ -n "$DOCUMENT_ROOT" ]
-then
-    sed -i 's/\/var\/www\/html/${DOCUMENT_ROOT}/g' /etc/httpd/conf/httpd.conf
-fi
-
-httpd -D FOREGROUND
---
+++ README.md
@@ -0,0 +1,5 @@
+# Dockerfiles
+
+![example workflow](https://github.com/ggMartinez/Dockerfiles/actions/workflows/full.yml/badge.svg)
+
+Just some dockerfiles for stuff. 
